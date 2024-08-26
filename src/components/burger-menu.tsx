8960b7e002073888968930b10c0a52170e840f6c
Eldiar <eldiar@MacBook-Air-Eldiar.local>
2024-08-27 01:03:46 +0600

    Update

--- src/components/burger-menu.tsx
+++ src/components/burger-menu.tsx
@@ -23,11 +23,11 @@ export default function BurgerMenu({options}: BurgerMenuProps) {
     <Hamburger toggled={isOpen} toggle={setIsOpen} />
     {
      isOpen &&  
-     <nav className='absolute top-16 right-5 bg-white p-4 min-w-[400px] h-[500px] z-50'>
-        <ul className='flex flex-col justify-center items-center gap-y-8 h-full w-full'>
+     <nav className='absolute top-16 right-5 bg-white p-4 min-w-[200px] h-[400px] z-50'>
+        <ul className='flex flex-col justify-center items-center gap-y-4 h-full w-full'>
         { options.map( (option) => {
             return <Link href={option.link} key={option.link}>
-           <li onClick={handleClose} className={`text-3xl cursor-pointer font-bold transition hover:text-main_third/90 hover:border-b-2 pt-1 hover:border-main_third  
+           <li onClick={handleClose} className={`text-2xl cursor-pointer font-bold transition hover:text-main_third/90 hover:border-b-2 pt-1 hover:border-main_third  
             ${option.active ? 'border-b-2 border-main_third text-main_third' : 'border-b-2 border-transparent'}`} key={option.link}>{option.name.toUpperCase()}</li>
             </Link>
         })}
