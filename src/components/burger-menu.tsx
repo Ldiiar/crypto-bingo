2979958df8588a146b99b5dee0bb73941fc5df70
Eldiar <eldiar@MacBook-Air-Eldiar.local>
2024-08-27 00:53:53 +0600

    Update

--- src/components/burger-menu.tsx
+++ src/components/burger-menu.tsx
@@ -1,13 +1,39 @@
 'use client'
 
 import Hamburger from 'hamburger-react'
+import Link from 'next/link'
 import { useState } from 'react'
 
-export default function BurgerMenu() {
+type BurgerMenuProps = {
+    options:  {
+        name: string,
+        link: string,
+        active: boolean
+      }[]
+}
+
+export default function BurgerMenu({options}: BurgerMenuProps) {
     const [isOpen, setIsOpen] = useState(false)
+    function handleClose() {
+        setIsOpen(prevState => !prevState)
+    }
+
   return (
-    <>
+    <section className=''>
     <Hamburger toggled={isOpen} toggle={setIsOpen} />
-    </>
+    {
+     isOpen &&  
+     <nav className='absolute top-16 right-5 bg-white p-4 min-w-[400px] h-[500px] z-50'>
+        <ul className='flex flex-col justify-center items-center gap-y-8 h-full w-full'>
+        { options.map( (option) => {
+            return <Link href={option.link}>
+           <li onClick={handleClose} className={`text-3xl cursor-pointer font-bold transition hover:text-main_third/90 hover:border-b-2 pt-1 hover:border-main_third  
+            ${option.active ? 'border-b-2 border-main_third text-main_third' : 'border-b-2 border-transparent'}`} key={option.link}>{option.name.toUpperCase()}</li>
+            </Link>
+        })}
+        </ul>
+    </nav>
+    }
+    </section>
   )
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -28,14 +28,16 @@ export default function Header() {
     <header className='flex w-full h-20 items-center bg-main_primary ' >
         <div className="flex w-full max-w-[1300px] m-auto text-main_fourth justify-between items-center pl-4 pr-4">
             <Logo/>
-                <ul className='hidden lg:flex gap-8'>
+            <nav className='hidden lg:flex'>
+                <ul className='flex gap-8'>
                  { options.map( (option) => {
-                    return <li className={`cursor-pointer font-medium transition hover:text-main_third/90 hover:border-b-2 pt-1 hover:border-main_third  
-                      ${option.active ? 'border-b-2 border-main_third text-main_third' : 'border-b-2 border-transparent'}`} key={option.link}>{option.name}</li>
+                   return <li className={`cursor-pointer font-medium transition hover:text-main_third/90 hover:border-b-2 pt-1 hover:border-main_third  
+                    ${option.active ? 'border-b-2 border-main_third text-main_third' : 'border-b-2 border-transparent'}`} key={option.link}>{option.name}</li>
                   })}
                 </ul>
+            </nav>
                 <div className="lg:hidden">
-                  <BurgerMenu />
+                  <BurgerMenu options={options}/>
                 </div>
         </div>
     </header>
