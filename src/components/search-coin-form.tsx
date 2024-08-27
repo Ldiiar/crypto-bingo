7b6ffc348cc084add01cd6ccfce332e879432630
Eldiar <eldiar@MacBook-Air-Eldiar.local>
2024-08-27 16:51:31 +0600

    Update

--- src/components/search-coin-form.tsx
+++ src/components/search-coin-form.tsx
@@ -5,6 +5,7 @@ import { Input } from './ui/input';
 import { useDispatch } from 'react-redux';
 import { updateCoinsMarket, updateSearchingPrompt } from '@/features/Coins/coinsSlice';
 import { ChangeEvent, useState } from 'react';
+import { IoSearch } from "react-icons/io5";
 
 export default function SearcCoinForm() {
 		const dispatch = useDispatch()
@@ -12,16 +13,19 @@ export default function SearcCoinForm() {
 		function handleTextChange(e: ChangeEvent<HTMLInputElement>) {
 			setInputText(e.target.value)
 			if (e.target.value === '') {
-				dispatch(updateSearchingPrompt(null))
+				// dispatch(updateSearchingPrompt(null))
 			} else {
-				dispatch(updateSearchingPrompt(e.target.value))
+				// dispatch(updateSearchingPrompt(e.target.value))
 			}
 		}
 
+		function SearchIcon() {
+			
+		}
 
 	return (
 		<>
-		<form className='flex w-full sm:w-2/3 gap-2'>
+		<form className='flex w-full sm:w-2/3 gap-2' onSubmit={() => SearchIcon()}>
 			<Input
 				placeholder='Search crypto by name'
 				id='coinName'
@@ -29,6 +33,10 @@ export default function SearcCoinForm() {
 				value={inputText}
 				onChange={(e) => handleTextChange(e)}
 			/>
+			<Button type='submit' className='bg-main_fourth'>
+				Search
+				<IoSearch className='ml-1'/>
+			</Button>
 		</form>
 		</>
 	);
