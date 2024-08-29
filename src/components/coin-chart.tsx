5f8db08ffdf77c3ac8d95b3bebe05621f745f78a
Eldiar <ldrmac07@gmail.com>
2024-08-29 23:14:19 +0600

    Update

--- src/components/coin-chart.tsx
+++ src/components/coin-chart.tsx
@@ -100,11 +100,8 @@ export function CoinChart() {
         </ChartContainer>
       </CardContent>
       <CardFooter className="flex-col items-start gap-2 text-sm">
-        <div className="flex gap-2 font-medium leading-none">
-          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-        </div>
         <div className="leading-none text-muted-foreground">
-          Showing prices for the last 6 months
+          Showing prices for the last 1 months
         </div>
       </CardFooter>
     </Card>
